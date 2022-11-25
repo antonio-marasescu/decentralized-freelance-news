7c64a7828f41d2b860211d551c7206afe0bb8c5e
Emperor <antonio.marasescuduran@gmail.com>
2022-11-25 22:20:03 +0200

    feat: styling of navigation

--- apps/main-ui-app/src/app/core/components/containers/navigation-container.component.ts
+++ apps/main-ui-app/src/app/core/components/containers/navigation-container.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'dfn-main-navigation-container',
-  template: `<dfn-main-navigation-view></dfn-main-navigation-view>`,
+  template: `<dfn-main-navigation-view><ng-content></ng-content></dfn-main-navigation-view>`,
   styleUrls: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- apps/main-ui-app/src/app/core/components/presentational/navigation-view/navigation-view.component.ts
+++ apps/main-ui-app/src/app/core/components/presentational/navigation-view/navigation-view.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'dfn-main-navigation-view',
   templateUrl: 'navigation-view.component.html',
   styleUrls: ['navigation-view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class NavigationViewComponent {}
